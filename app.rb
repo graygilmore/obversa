75089c2154d377f1a920d97e212ba0810f5a8ca9
Gray Gilmore <graygilmore@gmail.com>
2016-04-06 17:38:29 -0700

    Add first route

--- app.rb
+++ app.rb
@@ -2,5 +2,8 @@ require "sinatra"
 
 module Obversa
   class Application < Sinatra::Base
+    get "/" do
+      erb :index
+    end
   end
 end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require "./app"
+
+Obversa::Application.run!
