329d85b51ebde8b6caa4171f37c99bec15e3d7b5
Gray Gilmore <graygilmore@gmail.com>
2016-04-07 20:30:34 -0700

    Create a small wrapper around Faraday's conn
    Rename the `api` method to `conn`, since we can't modify the request
    parameters directly on the initial faraday instantiation. To work around
    this, we make the new `api` method take two things: a method symbol and
    a block. The method symbol must match a supported method on Faraday, and
    then the block is called with the request where we can eventually also
    inject an authorization header.
    
    With help and words from @alexblackie.

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra/base"
+require "faraday"
 
 module Obversa
   class Application < Sinatra::Base
     get "/" do
       erb :index
+    end
+
+    private
+
+    def api(method, &block)
+      conn.send(method) do |r|
+        if method == :post
+          r.headers["Content-Type"] = "application/json"
+        end
+        block.call(r)
+      end
+    end
+
+    def conn
+      Faraday.new(url: "#{api_url}")
+    end
+
+    def api_url
+      ENV["API_HOST"] || "http://localhost:9494"
     end
   end
 end
